ecc6243092715fdebc510f56fd077427bfb38779
ethandawkins <51774290+ethandawkins@users.noreply.github.com>
2019-06-14 11:47:12 +1000

    Add files via upload

--- IST Term 2.py
+++ IST Term 2.py
@@ -1,4 +1,4 @@
-##Gets player name and saves it as a variable.
+gameRun = True
 print(r"""
  ____________________________________________________________________
 /  ________________________________________________________________  \
@@ -23,50 +23,57 @@ print(r"""
 \____________________________________________________________________/
                                    
 """)
-playerName = input('What is your name? ')
-gameRange = int(input('What game range? 10,20 or 50? '))
-this = [10,20,50]
-alphabet = ['abcdefghijklmnopqrstuvwxyz']
-while gameRange not in this:
-    print('Game range has to be 10,20 or 50!')
+
+while(gameRun == True):
+    gameRuns = str(input('\nWhat would you like to do? 1 or 2?\n\n1. Play Number Game\n2. Exit\n\n'))
+    if(gameRuns == 1):
+        gameRun = gameRun
+    if(gameRuns == 2):
+        gameRun = False
+    playerName = input('What is your name? ')
     gameRange = int(input('What game range? 10,20 or 50? '))
-    if gameRange in this:
-        break
-import random
-gameNum = random.randint(1,gameRange)
-playerGuess = ' '
-guessesTaken = 0
-while playerGuess != gameNum:
-    print('Guess a number between 1 and' ,gameRange)
-    playerGuess = int(input())
-    guessesTaken += 1
-    if(playerGuess < gameNum):
-        print('Too low')
-    if(playerGuess > gameNum):
-        print('Too high')
-    if(playerGuess == gameNum):
-        print('Correct!')
-        break
-if(gameRange == 10):
-    if(guessesTaken <= 3):
-        print('Level chosen: Easy\nThe number was',gameNum,'\nGuesses Taken:',guessesTaken,'\nMarvellous',playerName)
-    if(4 <= guessesTaken <= 7):
-        print('Level chosen: Easy\nThe number was',gameNum,'\nGuesses Taken:',guessesTaken,'\nQuite good',playerName)
-    if(guessesTaken > 7):
-        print('Level chosen: Easy\nThe number was',gameNum,'\nGuesses Taken:',guessesTaken,'\nKeep trying',playerName)
+    this = [10,20,50]
+    alphabet = ['abcdefghijklmnopqrstuvwxyz']
+    while gameRange not in this:
+        print('Game range has to be 10,20 or 50!')
+        gameRange = int(input('What game range? 10,20 or 50? '))
+        if gameRange in this:
+            break
+    import random
+    gameNum = random.randint(1,gameRange)
+    playerGuess = ' '
+    guessesTaken = 0
+    while playerGuess != gameNum:
+        print('Guess a number between 1 and' ,gameRange)
+        playerGuess = int(input())
+        guessesTaken += 1
+        if(playerGuess < gameNum):
+            print('Too low')
+        if(playerGuess > gameNum):
+            print('Too high')
+        if(playerGuess == gameNum):
+            print('Correct!')
 
-if(gameRange == 20):
-    if(guessesTaken <= 7):
-        print('Level chosen: Medium\nThe number was',gameNum,'\nGuesses Taken:',guessesTaken,'\nGreat work',playerName)
-    if(8 <= guessesTaken <= 15):
-        print('Level chosen: Medium\nThe number was',gameNum,'\nGuesses Taken:',guessesTaken,'\nAdequate',playerName)
-    if(guessesTaken > 15):
-        print('Level chosen: Medium\nThe number was',gameNum,'\nGuesses Taken:',guessesTaken,'\nShabby',playerName)
+            if(gameRange == 10):
+                if(guessesTaken <= 3):
+                    print('Level chosen: Easy\nThe number was',gameNum,'\nGuesses Taken:',guessesTaken,'\nMarvellous',playerName)
+                if(4 <= guessesTaken <= 7):
+                    print('Level chosen: Easy\nThe number was',gameNum,'\nGuesses Taken:',guessesTaken,'\nQuite good',playerName)
+                if(guessesTaken > 7):
+                    print('Level chosen: Easy\nThe number was',gameNum,'\nGuesses Taken:',guessesTaken,'\nKeep trying',playerName)
 
-if(gameRange == 50):
-    if(guessesTaken <= 20):
-        print('Level chosen: Hard\nThe number was',gameNum,'\nGuesses Taken:',guessesTaken,'\nOut of this world',playerName)
-    if(21 <= guessesTaken <= 40):
-        print('Level chosen: Hard\nThe number was',gameNum,'\nGuesses Taken:',guessesTaken,'\nPretty good',playerName)
-    if(guessesTaken > 40):
-        print('Level chosen: Hard\nThe number was',gameNum,'\nGuesses Taken:',guessesTaken,'\nOh NO',playerName)
+            if(gameRange == 20):
+                if(guessesTaken <= 7):
+                    print('Level chosen: Medium\nThe number was',gameNum,'\nGuesses Taken:',guessesTaken,'\nGreat work',playerName)
+                if(8 <= guessesTaken <= 15):
+                    print('Level chosen: Medium\nThe number was',gameNum,'\nGuesses Taken:',guessesTaken,'\nAdequate',playerName)
+                if(guessesTaken > 15):
+                    print('Level chosen: Medium\nThe number was',gameNum,'\nGuesses Taken:',guessesTaken,'\nShabby',playerName)
+
+            if(gameRange == 50):
+                if(guessesTaken <= 20):
+                    print('Level chosen: Hard\nThe number was',gameNum,'\nGuesses Taken:',guessesTaken,'\nOut of this world',playerName)
+                if(21 <= guessesTaken <= 40):
+                    print('Level chosen: Hard\nThe number was',gameNum,'\nGuesses Taken:',guessesTaken,'\nPretty good',playerName)
+                if(guessesTaken > 40):
+                    print('Level chosen: Hard\nThe number was',gameNum,'\nGuesses Taken:',guessesTaken,'\nOh NO',playerName)
